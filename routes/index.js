8aa7007e24e4cecce97d978c36daf5b9e8513191
Nicolai Safai <nicolaisafai@gmail.com>
2019-02-13 23:37:28 -0800

    polished page styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Make Chat' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Chat Box' });
 });
 
 module.exports = router;
